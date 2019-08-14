b8b06345ad02cbcbc4930e94d2e620fdfb577855
Thorsten Kukuk <kukuk@thkukuk.de>
2019-08-14 18:14:21 +0200

    Add MicroOS NTP Configuration Dialog [bsc#1135342]

---
+++ src/lib/y2caasp/clients/microos_role_dialog.rb
@@ -0,0 +1,27 @@
+require "yast"
+
+# so far the Kubic dialog is the same as in CaaSP,
+# just with different title and defaults
+require "y2caasp/clients/admin_role_dialog"
+
+module Y2Caasp
+  # This library provides a simple dialog for setting
+  # the kubeadm role specific settings:
+  #   - the NTP server names
+  class MicroOSRoleDialog < AdminRoleDialog
+    def initialize
+      textdomain "caasp"
+      super
+    end
+
+    #
+    # The dialog title
+    #
+    # @return [String] the title
+    #
+    def title
+      # TRANSLATORS: dialog title
+      _("NTP Configuration")
+    end
+  end
+end
